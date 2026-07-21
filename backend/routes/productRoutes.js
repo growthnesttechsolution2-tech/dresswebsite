23d1061f50f4faa927ded1d637336257b6e068d4
growthnest <growthnesttechsolution2@gmail.com>
2026-07-21 12:30:16 +0530

    Added new features

--- backend/routes/productRoutes.js
+++ backend/routes/productRoutes.js
@@ -3,9 +3,9 @@ const controller = require("../controllers/productController");
 const upload = require("../middleware/upload");
 const { adminProtect } = require("../middleware/auth");
 
-router.route("/").post(adminProtect, upload.array("images", 5), controller.createProduct).get(controller.getProducts);
+router.route("/").post(adminProtect, upload.array("images", 10), controller.createProduct).get(controller.getProducts);
 router.post("/bulk", adminProtect, controller.bulkCreateProducts);
 router.get("/category/:category", controller.byCategory);
-router.route("/:id").get(controller.getProduct).put(adminProtect, upload.array("images", 5), controller.updateProduct).delete(adminProtect, controller.deleteProduct);
+router.route("/:id").get(controller.getProduct).put(adminProtect, upload.array("images", 10), controller.updateProduct).delete(adminProtect, controller.deleteProduct);
 
 module.exports = router;
